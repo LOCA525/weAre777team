52268f3a15306d55ed454aa7520d3cc86e03e5eb
박준영 <pjy93525@gmail.com>
2023-05-16 16:26:36 +0900

    3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           hihihiihifffhi <code>src/App.js</code> and save to reload.
+          <div className="지훈">12</div>
+          <div className="정환">34</div>
+          <div className="도영">56</div>
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
